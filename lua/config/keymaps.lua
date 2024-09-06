217e58ba030c0161fcdf250f7d77596741071014
sinisa <42147156+kraangprime@users.noreply.github.com>
2024-09-06 21:10:59 +0200

    Update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,10 @@
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down" })
+
+vim.keymap.set("n", "<Leader>rx", ":split | term ruby %<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>re", ":split | term ruby % ", { noremap = true, silent = false })
+
+vim.keymap.set("n", "<Leader>rbx", ":split | term bundle exec %<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>rbi", ":split | term bundle install <CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>rbp", ":split | term bundle package <CR>", { noremap = true, silent = true })
